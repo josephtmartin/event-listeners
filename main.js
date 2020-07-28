61b2851d60038db2592d56dc1436885cb3ff2247
josephtmartin <goldglovejo13@gmail.com>
2020-07-27 19:02:11 -0500

    callbacks and event listeners

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-"use strict";
+'use strict';
+
+const function1 = (str) => {
+  return `Hola! ${str}`;
+};
+
+const function2 = (str) => {
+  return `Adios! ${str}`;
+};
+
+const callBackFunction = (str, func) => {
+  return func(str);
+};
+
+// console.log(callBackFunction('1', function1));
+// console.log(callBackFunction('2', function2));
+
+const myBtn = document.getElementById('main-btn');
+// console.log(myBtn);
+
+const otherBtn = document.querySelector('#main-btn');
+// console.log(otherBtn);
+
+// div.addEventListener(click, () => {
+//   console.log('clicked button!');
+// });
+
+const youClickedMe = () => {
+  console.log('You clicked me!');
+};
+
+otherBtn.addEventListener('click', youClickedMe);
